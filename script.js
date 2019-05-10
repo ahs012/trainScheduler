bdb75c55bd6f4e46299cf9b8231dd2f79472e46f
Smoss <ahs012@gmail.com>
2019-05-10 18:38:46 -0400

    Initial Firebase Set Up

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
-$document.ready(function(){
+$(document).ready(function(){
     //Firebase DB Connection 
     //Need Help setting up firebase DB, forgot 100% of it. Can finish homewokr then!
+    
+
+    // Initialize Firebase
+    // import * as firebase from 'firebase';
+    var firebaseConfig = {
+        apiKey: "AIzaSyDb2xw0yAsjquYLxSl2QW2gZUmh44nzrlg",
+        authDomain: "trainscheduler-45e93.firebaseapp.com",
+        databaseURL: "https://trainscheduler-45e93.firebaseio.com",
+        projectId: "trainscheduler-45e93",
+        storageBucket: "trainscheduler-45e93.appspot.com",
+        messagingSenderId: "545106633100",
+        appId: "1:545106633100:web:40b1c88fa13e690e"
+      };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
+
     var trainName = "";
     var destination = "";
     var time = "";
     var frequency = "";
+
     //On Click Listener for Adding Train
-    $( "#addTrain" ).click(function() {
+    $( "#addTrain" ).click(function(event) {
+        event.preventDefault();
         //Grab User Inputs
         var trainName = $("#trainInput").val().trim();
         var destination = $("#destinationInput").val().trim();
         var time = $("#timeInput").val().trim();
         var frequency = $("#frequencyInput").val().trim();
-        // Var to hold inputed data
-        var trainInput = {
+
+    // Push trainInput object to DB
+        database.ref().push({
             name: trainName,
             destination: destination,
             time: time,
             frequency: frequency
-        }
-        // Push trainInput object to DB
-
-        //Clear Input Data
+        });
+    //Clear Input Data
         $("#trainInput").val("");
         $("#destinationInput").val("");
         $("#frequencyInput").val("");
         $("#timeInput").val("");
       });
-      //Get Train Data from Firebase DB
-      //Append Train Data to Table with a loop ?
-      //
+    //Get Train Data from Firebase DB
+    //Append Train Data to Table with a loop ?
+      
+    //on value snapshot <---
+    //everytime you add, take snapshot and retrieve info
+    //   snapshot.trainname etc
+
 
       
 })
